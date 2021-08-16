e4838d7860a50e20e696088223ad81d060448120
이재호 <kojotourist@gmail.com>
2021-08-16 19:41:51 +0900

    logger

--- dist/utils/log-cloudwatch.js
+++ dist/utils/log-cloudwatch.js
@@ -4,31 +4,29 @@ exports.logQuarterCandle = exports.logDayCandle = void 0;
 var logger_1 = require("./logger");
 function logDayCandle(dayCandle, ema, market) {
     if (market === void 0) { market = ''; }
-    var logger = market === 'ethereum' ? logger_1.etherLogger : 'alpha' ? logger_1.alphaLogger : logger_1.etherLogger;
     // start line
-    logger.info('********* Lets start the game Confess ! *************');
-    logger.info("====== trying to fetch dayCandle ======");
-    logger.info("time: " + new Date(Date.now()).toISOString() + " ");
-    logger.info("====== fetched data ======");
+    logger_1.logger.info('********* Lets start the game Confess ! *************');
+    logger_1.logger.info("====== trying to fetch dayCandle ======");
+    logger_1.logger.info("time: " + new Date(Date.now()).toISOString() + " ");
+    logger_1.logger.info("====== fetched data ======");
     for (var _i = 0, _a = Object.entries(dayCandle); _i < _a.length; _i++) {
         var _b = _a[_i], key = _b[0], value = _b[1];
-        logger.info(key + " : " + value);
+        logger_1.logger.info(key + " : " + value);
     }
-    logger.info("====== calculated ema ======");
-    logger.info("ema: " + ema);
+    logger_1.logger.info("====== calculated ema ======");
+    logger_1.logger.info("ema: " + ema);
 }
 exports.logDayCandle = logDayCandle;
 function logQuarterCandle(quarterCandle, market) {
     if (market === void 0) { market = ''; }
-    var logger = market === 'ethereum' ? logger_1.etherLogger : 'alpha' ? logger_1.alphaLogger : logger_1.etherLogger;
     // start line
-    logger.info('********* Lets start the game Confess ! *************');
-    logger.info("====== trying to fetch quarterCandle ======");
-    logger.info("time: " + new Date(Date.now()).toISOString() + " ");
-    logger.info("====== fetched data ======");
+    logger_1.logger.info('********* Lets start the game Confess ! *************');
+    logger_1.logger.info("====== trying to fetch quarterCandle ======");
+    logger_1.logger.info("time: " + new Date(Date.now()).toISOString() + " ");
+    logger_1.logger.info("====== fetched data ======");
     for (var _i = 0, _a = Object.entries(quarterCandle); _i < _a.length; _i++) {
         var _b = _a[_i], key = _b[0], value = _b[1];
-        logger.info(key + " : " + value);
+        logger_1.logger.info(key + " : " + value);
     }
 }
 exports.logQuarterCandle = logQuarterCandle;
