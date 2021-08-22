1cb6f753488d3575320551c6d4b0f2e7c813f5fa
이재호 <kojotourist@gmail.com>
2021-08-22 16:58:13 +0900

    put bitcoin data

--- src/bin/put-data.ts
+++ src/bin/put-data.ts
@@ -2,10 +2,12 @@ import dataHandler from '../core/data-handler';
 
 // put data
 (async () => {
-  // const result = await dataHandler.putDataWithEms({year: 2021, month: 7, date:12}, {year:2021, month:7, date:13}, 'ethereum');
-  // const result = await dataHandler.putTenData({year: 2020, month: 10, date:18}, 'alpha');
+  // const result = await dataHandler.putDataWithEms({year: 2017, month: 9, date:6}, {year:2021, month:8, date:23});
+  // const result = await dataHandler.putDataWithEms({year: 2017, month: 9, date:6}, {year:2017, month:9, date:10});
+  // const result = await dataHandler.putTenData({year: 2017, month: 8, date:26});
+  // const result = await dataHandler.putDataWithAverEms({year: 2017, month: 9, date:6});
   // const result = await dataHandler.putDataWi020, month: 10, date:28}, 'alpha');
-  const result = await dataHandler.putDataQuarterly({year: 2021, month: 7, date:20, hour: 16}, {year:2021, month:7, date:21, hour: 0});
+  const result = await dataHandler.putDataQuarterly({year: 2021, month: 7, date:20, hour: 16}, {year:2021, month:7, date:22, hour: 8});
   // const result = await dataHandler.putDataToExcel();
 
   // const today = new Date(Date.UTC(2021, 5, 20));
